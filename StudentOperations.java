acab70cdea370033fe66496e611adfadfe602692
Vraj Patel <vrajbpatel1719@gmail.com>
2025-04-20 00:17:09 +0530

    Initialize StudentOperations class and constructor to create an empty list of students

---
+++ StudentOperations.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+ 
+ // This class handles all student-related operations like adding, searching, updating, and deleting students.
+ public class StudentOperations {
+     ArrayList<Student> students; // List to store Student objects
+ 
+     // Constructor: Initializes an empty list of students.
+     public StudentOperations() {
+         this.students = new ArrayList<>();
+     }
+ 
+ 
+ }
